8e7d087b8e89671c33e02ff915a97dfdc9849482
Szymon Ratajczyk <furusenpai@gmail.com>
2016-05-26 22:20:53 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,52 @@
+// Add variables to Gulp
 
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    jade = require('gulp-jade'),
+    sass = require('gulp-sass');
+    
+//.end variables
+    
+//Gulp make http server 
 
 gulp.task('connect', function() {
     connect.server({
-      root: '',
-      livereload: true
+        root: '',
+        livereload: true
     });
 });
 
-gulp.task('default', ['connect']);
+//.end of http server
+
+//Gulp learn Jade
+
+gulp.task('jade', function() {
+    gulp.src('./jade/**/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./'))
+});
+
+//.end of learn Jade
+
+//Gulp learn Sass
+
+gulp.task('sass', function() {
+
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+//Gulp watch Sass
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+//.end watch Sass
+
+//.end of learn Sass
+
+gulp.task('default', ['connect', 'jade', 'sass']);
