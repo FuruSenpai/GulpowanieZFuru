1b7763df4f72d091b3f72891b161053a4237a8b5
Szymon Ratajczyk <furusenpai@gmail.com>
2016-05-26 21:27:01 +0200

    gulp + gulp-connect included
    Added gulp + gulp-connect in default task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
-// here will be gulpfile.js
+
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+    connect.server({
+      root: '',
+      livereload: true
+    });
+});
+
+gulp.task('default, ['connect']);
